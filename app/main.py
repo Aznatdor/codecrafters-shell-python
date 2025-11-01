6d40b3c9f177c2508e48ac94f755b64d7089981d
Aznatdor <abobrov333@gmail.com>
2025-11-01 20:08:22 +0200

    Simple echo function

--- app/main.py
+++ app/main.py
@@ -3,14 +3,17 @@ import sys
 def main():
     while True:
         sys.stdout.write("$ ")
-        arguments = input()
-        argumentsList = arguments.split()   # simple parsing
-        command = argumentsList[0]
+        rawArguments = input()
+
+        spaceInd = rawArguments.find(" ")
+        command, arguments = rawArguments[:spaceInd], rawArguments[spaceInd+1:]
 
         if command == "exit":
             sys.exit(0)
-
-        print(f"{arguments}: not found")
+        elif command == "echo":
+            print(arguments)
+        else:
+            print(f"{arguments}: not found")
 
 
 if __name__ == "__main__":
